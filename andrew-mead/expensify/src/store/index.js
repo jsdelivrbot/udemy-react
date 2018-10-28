0ec176f39d5cc5002a212d18cdea72d8307f75a4
mariobrusarosco <mariobrusarosco@hotmail.com>
2018-10-28 16:13:05 -0300

    Andrew course: progress untill lecture 97

--- andrew-mead/expensify/src/store/index.js
+++ andrew-mead/expensify/src/store/index.js
@@ -1,33 +1,86 @@
-import { createStore } from 'redux'
+import { createStore, combineReducers, dispatch } from 'redux'
+import uuid from 'uuid'
 
-const initState = { counter: 10 }
 
-const incrementCount = n => {
-  return {
-    type: 'INCREMENT'
+const initialState = {
+  expenses: [],
+  filters: {
+    text: 'rent',
+    sortBy: 'amount',
+    startDate: null,
+    endDate: null,
+  },
+}
+
+
+const expensesReducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_EXPENSE:
+      return [
+        action.expense,
+        ...state,
+      ]
+    case REMOVE_EXPENSE:
+      return state.filter(expense => expense.id !== action.id)    
+    default:
+      return state  
   }
 }
-  
 
-const store = createStore((state = initState, action) => {
+const filtersReducers = (state = [], action) => {
   switch (action.type) {
-    case 'RESET':
-      return { counter: 0 }
-    case 'INCREMENT':
-      return { counter: ++state.counter }
     default:
       return state
   }
+}
+
+const reducers = combineReducers({
+  expenses: expensesReducer,
+  filters: filtersReducers,
+})
+
+const store = createStore(reducers, initialState)
+  store.subscribe(() => console.log(store.getState()))
+
+
+const ADD_EXPENSE = 'ADD_EXPENSE'
+
+const addExpense = ({ description, note, amount, createAt } = {}) => ({
+  type: ADD_EXPENSE,
+  expense: {
+    id: uuid(),
+    description, 
+    note, 
+    amount, 
+    createAt
+  }
+})
+
+const REMOVE_EXPENSE = 'REMOVE_EXPENSE'
+
+const removeExpense = id => ({
+  type: REMOVE_EXPENSE,
+  id
 })
 
 
-store.subscribe(() => {
-  console.log('store changed. state: ', store.getState())
-})
 
-store.dispatch(incrementCount(1))
+const ExpenseOne = store.dispatch(addExpense({
+  description: '11111',
+  note: null,
+  amount: null,
+  createdAt: null,
+  }
+))
 
-store.dispatch({
-  type: 'RESET'
-})
 
+
+store.dispatch(addExpense({
+  description: '222222',
+  note: null,
+  amount: null,
+  createdAt: null,
+  }
+))
+
+store.dispatch(removeExpense(ExpenseOne.expense.id))
